7815ad237aea7581c62c2e627037223de68b1181
Vladimir Che <vladimirchukarev@gmail.com>
2021-03-10 13:43:59 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a, b) => a + b, 0) / array.length;
+};
